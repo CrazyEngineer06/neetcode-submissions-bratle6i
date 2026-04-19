09108a2ac208829909126e56d3cd5bf9de9d4ab0
Piyush <Crazyengineer006@gmail.com>
2026-04-19 10:21:24 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = nums[0];
+
+        int n = nums.length;
+        
+        for(int i=1;i<n;i++){
+            ans ^= nums[i];
+        }
+
+        return ans;
+    }
+}

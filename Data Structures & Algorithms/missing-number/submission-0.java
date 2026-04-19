1e171e1fc1cacc3ce8ace9faad233a31de0c1a74
Piyush <Crazyengineer006@gmail.com>
2026-04-19 10:55:05 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int missing = 0;
+        int n = nums.length;
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+
+        for(int i=0;i<=n;i++){
+            if(!set.contains(i)){
+                return i;
+            }
+        }
+        return missing;
+    }
+}
